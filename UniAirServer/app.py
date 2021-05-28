5f1980c3c4156dd5057ce3c012ca934a6b26087d
Kidsnd274 <samuel_tee_gy@hotmail.com>
2021-05-29 02:31:15 +0800

    Added some API Calls - Added some API calls and API structure - Updated index.html for /test-ir

--- UniAirServer/app.py
+++ UniAirServer/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, jsonify
 from virtualcontroller import VirtualController
 
 app = Flask(__name__)
@@ -8,13 +8,30 @@ virtual_controller = VirtualController()
 def index():
     return render_template('index.html')
 
-@app.route('/test-ir/', methods=["POST"])
+@app.route('/test-ir', methods=["POST"])
 def test_ir():
     virtual_controller.test_ir()
     return render_template('index.html')
 
-#@app.route('/web-api')
+# API Calls
+@app.route('/api/toggle-power')
+def toggle_power():
+    virtual_controller.toggle_power()
+    return app.response_class(status = 200)
 
+@app.route('/api/get_aircon_data', methods=["GET"])
+def get_aircon_data():
+    data = jsonify(
+        aircon_power = virtual_controller.aircon_power,
+        aircon_temp = virtual_controller.aircon_temp,
+        aircon_fanspeed = virtual_controller.aircon_fanspeed    
+    )
+    # response = app.response_class(
+    #     response = data,
+    #     status = 200,
+    #     mimetype = 'application/json'
+    # )
+    return data
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug = True, host = '0.0.0.0')
--- UniAirServer/virtualcontroller.py
+++ UniAirServer/virtualcontroller.py
@@ -5,6 +5,7 @@ class VirtualController():
     """Main virtual controller handling IR commands with LIRC and web server"""
 
     def __init__(self):
+        self.controller = "mitsubishi_kh18a"
         self.aircon_power = False
         self.aircon_temp = 26
         self.aircon_fanspeed = 2
@@ -22,6 +23,18 @@ class VirtualController():
     def test_ir(self):
         self.client.send_once("mitsubishi_kh18a", "SWITCH_OFF")
         print("DEBUG: Sent IR Test, SWITCH_OFF from mitsubishi_kh18a")
+
+    def toggle_power(self):
+        if self.aircon_power == False:
+            self.client.send_once(self.controller, "SWITCH_ON")
+            print("DEBUG: Sent IR Test, SWITCH_ON from mitsubishi_kh18a")
+            self.aircon_power = True
+        elif self.aircon_power == True:
+            self.client.send_once(self.controller, "SWITCH_OFF")
+            print("DEBUG: Sent IR Test, SWITCH_OFF from mitsubishi_kh18a")
+            self.aircon_power = False
+        else:
+            pass
 
 if __name__ == "__main__":
     print("Please run main webserver app.py!")
