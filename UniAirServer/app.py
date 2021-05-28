2d98079aa31f8b1b037fb3f0c7ff60ecee191fda
Kidsnd274 <samuel_tee_gy@hotmail.com>
2021-05-29 05:32:55 +0800

    Added API methods for aircon data - Updated API methods, can handle json now - Added two methods in virtual controller to read and update aircon data - Added some aircon controls in the virtual controller

--- UniAirServer/app.py
+++ UniAirServer/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, url_for, jsonify
+from flask import Flask, render_template, url_for, request, abort
 from virtualcontroller import VirtualController
+from datetime import datetime
 
 app = Flask(__name__)
 virtual_controller = VirtualController()
@@ -19,19 +20,35 @@ def toggle_power():
     virtual_controller.toggle_power()
     return app.response_class(status = 200)
 
-@app.route('/api/get_aircon_data', methods=["GET"])
+@app.route('/api/aircon_data', methods=["GET", "POST"])
 def get_aircon_data():
-    data = jsonify(
-        aircon_power = virtual_controller.aircon_power,
-        aircon_temp = virtual_controller.aircon_temp,
-        aircon_fanspeed = virtual_controller.aircon_fanspeed    
-    )
-    # response = app.response_class(
-    #     response = data,
-    #     status = 200,
-    #     mimetype = 'application/json'
+    if request.method == 'GET':
+        response = app.response_class(
+            response = virtual_controller.get_aircon_data(),
+            mimetype = 'application/json'
+        )
+        return response
+    elif request.method == 'POST':
+        if not request.json:
+            abort(400)
+        else:
+            virtual_controller.update_aircon_data(request.json)
+            response = app.response_class(
+                response = virtual_controller.get_aircon_data(),
+                mimetype = 'application/json'
+            )
+            return response
+    else:
+        print("DEBUG: Unknown HTTP Method")
+        pass
+
+
+    # data = jsonify(
+    #     date_and_time = datetime.now().isoformat(), # ISO 8601 format, find function to decode this format for JS
+    #     aircon_power = virtual_controller.aircon_power,
+    #     aircon_temp = virtual_controller.aircon_temp,
+    #     aircon_fanspeed = virtual_controller.aircon_fanspeed    
     # )
-    return data
 
 if __name__ == "__main__":
     app.run(debug = True, host = '0.0.0.0')
