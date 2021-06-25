88908cbcade9bd385d39c0ecaaec5ca3271f2947
WeiJunAng <ang.weijun1999@gmail.com>
2021-06-25 21:52:50 +0800

    Commit
    Improve Aesthetics of Main Page

--- AppDev/UniAirApp/screens/Welcome.js
+++ AppDev/UniAirApp/screens/Welcome.js
@@ -2,93 +2,109 @@ import React from "react";
 import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
 
 const Welcome = (props) => {
-  return <View style = {styles.container}>
-    <View style= {styles.subContainerTop}>
+  return (
+    <View style={styles.container}>
+      <View style={styles.subContainerTop}>
+       <Text style={styles.tagLine}>UNI</Text><View style = {styles.tagLine2Container}><Text style={styles.tagLine2}>AIR</Text></View>
+      </View>
+      <View style={styles.subContainerBottom}>
+        <TouchableOpacity
+          style={styles.buttonStyleLearnMore}
+          onPress={() => [props.navigation.navigate("LearnMore")]}
+        >
+          <Text style={styles.textLearnMore}>Learn More</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.buttonStyleRegister}
+          onPress={() => [props.navigation.navigate("Registration")]}
+        >
+          <Text style={styles.textRegister}>Register</Text>
+        </TouchableOpacity>
+      </View>
     </View>
-    <View style = {styles.subContainerBottom}>
-    <Text style = {styles.tagLine}>Hello</Text>
-    <TouchableOpacity style = {styles.buttonStyleLearnMore} onPress = {() => [props.navigation.navigate("LearnMore")]}>
-      <Text style ={styles.textLearnMore}>Learn More</Text>
-    </TouchableOpacity>
-    <TouchableOpacity style = {styles.buttonStyleRegister} onPress = {() => [props.navigation.navigate("Registration")]}>
-      <Text style = {styles.textRegister} >Register</Text>
-    </TouchableOpacity>
-    </View>
-  </View>;
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  
-  subContainerTop: {
-    flex:1,
-    flexDirection: 'row',
-    width: '100%',
-    backgroundColor: '#FFFF',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
+    backgroundColor: "#FFFF",
+    alignItems: "center",
+    justifyContent: "center",
   },
 
-  
+  subContainerTop: {
+    flex: 1,
+    flexDirection: "row",
+    width: "100%",
+    backgroundColor: "#FFFF",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
   subContainerBottom: {
-    flex:2,
-    backgroundColor: '#00B4D8',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    width: '100%',
-    padding: '10%',
-  }, 
-  
+    flex: 2,
+    backgroundColor: "#00B4D8",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    width: "100%",
+    padding: "10%",
+  },
+
   textRegister: {
-    textAlign: 'center',
-    alignItems:'center',
-    fontSize:20,
-    paddingTop:'2.2%',
-    color:'#00B4D8'
-  }, 
+    textAlign: "center",
+    alignItems: "center",
+    fontSize: 20,
+    color: "#00B4D8",
+  },
 
   tagLine: {
-    justifyContent:'flex-start',
-    textAlign: 'center',
-    alignItems:'center',
-    fontSize:20,
-    paddingBottom:'100%',
-    color:'black'
-  }, 
+    justifyContent: "flex-end",
+    textAlign: "center",
+    alignItems: "flex-end",
+    fontSize: 75,
+    color: "#00B4D8",
+  },
+
+  tagLine2: {
+    justifyContent: "flex-end",
+    textAlign: "center",
+    alignItems: "flex-end",
+    fontSize: 60,
+    color: "#FFFF",
+  },
+
+  tagLine2Container: {
+    backgroundColor: "#00B4D8"
+  },
 
   textLearnMore: {
-    textAlign: 'center',
-    alignItems:'center',
-    fontSize:20,
-    paddingTop:'2.2%',
-    color: '#FFFF'
-  }
-  ,
+    textAlign: "center",
+    alignItems: "center",
+    fontSize: 20,
+    paddingTop: "2.2%",
+    color: "#FFFF",
+  },
   buttonStyleLearnMore: {
-    flexDirection:'row',
-    backgroundColor: '#00B4D8',
+    flexDirection: "row",
+    backgroundColor: "#00B4D8",
     borderRadius: 30,
-    width: '75%',
-    height: '10%',
+    width: "75%",
+    height: "10%",
     margin: 10,
-    borderWidth:2,
-    borderColor: '#FFFF',
-    justifyContent:'center'
+    borderWidth: 2,
+    borderColor: "#FFFF",
+    justifyContent: "center",
   },
   buttonStyleRegister: {
-    flexDirection:'row',
-    backgroundColor: '#FFFF',
+    flexDirection: "row",
+    backgroundColor: "#FFFF",
     borderRadius: 30,
-    width: '75%',
-    height: '10%',
+    width: "75%",
+    height: "10%",
     margin: 10,
-    justifyContent:'center'
-  }
+    justifyContent: "center",
+  },
 });
 
 export default Welcome;
